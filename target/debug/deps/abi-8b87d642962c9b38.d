6724262d2e617b76b0d12d2ef05e32142a97717b
neal <neal0420yang@gmail.com>
2023-07-27 00:04:19 +0800

    the first is to clarify the problem, we ought to step by step define the reservation system, choose gRPC interface, and use protobuf from user's perspective define what method we have.

---
+++ target/debug/deps/abi-8b87d642962c9b38.d
@@ -0,0 +1,8 @@
+/Users/shin/Desktop/reservation_system/target/debug/deps/abi-8b87d642962c9b38.rmeta: abi/src/lib.rs Cargo.toml
+
+/Users/shin/Desktop/reservation_system/target/debug/deps/abi-8b87d642962c9b38.d: abi/src/lib.rs Cargo.toml
+
+abi/src/lib.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
---
+++ target/debug/deps/reservation-8855bf6b94652b8d.d
@@ -0,0 +1,8 @@
+/Users/shin/Desktop/reservation_system/target/debug/deps/reservation-8855bf6b94652b8d.rmeta: reservation/src/lib.rs Cargo.toml
+
+/Users/shin/Desktop/reservation_system/target/debug/deps/reservation-8855bf6b94652b8d.d: reservation/src/lib.rs Cargo.toml
+
+reservation/src/lib.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
